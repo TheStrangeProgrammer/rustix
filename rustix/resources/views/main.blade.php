2ccb8b8b5e8024ed2ad0d756d4cf3245c37031b2
ioneltomi <ioneltomi80@gmail.com>
2021-11-16 19:33:18 +0200

    asdf

---
+++ rustix/resources/views/main.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Rustix - @yield('title')</title>
+    </head>
+
+<body>
+@include('header')
+
+<div>
+    @yield('content')
+</div>
+@include('footer')
+
+
+</body>
+</html>
