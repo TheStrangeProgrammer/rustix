c40579ddc7c5936ef35d285b4a8f23df775b1b5a
ioneltomi <ioneltomi80@gmail.com>
2021-11-17 08:35:23 +0200

    as

--- rustix/resources/views/partials/left_sidebar.blade.php
+++ rustix/resources/views/partials/left_sidebar.blade.php
@@ -1,16 +1,16 @@
 
-<section class="d-flex h100">
+<section class="d-flex">
     @include('partials.left_sidebar.alt')
     @include('partials.left_sidebar.faq')
     @include('partials.left_sidebar.tos')
 
     <div class="d-flex flex-column">
         
-        <button>TOS</button>
-        <button>FAQ</button>
-        <button>ALT</button>
+        <button class="p-1">TOS</button>
+        <button class="p-1">FAQ</button>
+        <button class="p-1">ALT</button>
         
-        <button class="mt-auto"><i class="fas fa-arrow-left mb15"></i></button>
+        <button class="mt-auto p-1"><i class="fas fa-arrow-left mb15"></i></button>
     </div>
     @include('partials.left_sidebar.chat')
 </section>
