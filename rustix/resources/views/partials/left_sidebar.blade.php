fa6dde369aa8553be463b90b6f82ab2dea3e88a0
ioneltomi <ioneltomi80@gmail.com>
2021-11-17 08:13:57 +0200

    Update left_sidebar.blade.php

--- rustix/resources/views/partials/left_sidebar.blade.php
+++ rustix/resources/views/partials/left_sidebar.blade.php
@@ -1,5 +1,5 @@
 
-<section class="d-flex">
+<section class="d-flex h100">
     @include('partials.left_sidebar.alt')
     @include('partials.left_sidebar.faq')
     @include('partials.left_sidebar.tos')
@@ -10,7 +10,7 @@
         <button>FAQ</button>
         <button>ALT</button>
         
-        <button><i class="fas fa-arrow-left mb15"></i></button>
+        <button class="mt-auto"><i class="fas fa-arrow-left mb15"></i></button>
     </div>
     @include('partials.left_sidebar.chat')
 </section>
