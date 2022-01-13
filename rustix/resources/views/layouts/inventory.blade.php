b811735eb07aa4afc431a04b5f64100da400485c
TheStrangeProgrammer <48047563+TheStrangeProgrammer@users.noreply.github.com>
2022-01-13 15:24:21 +0200

    inventory

--- rustix/resources/views/layouts/inventory.blade.php
+++ rustix/resources/views/layouts/inventory.blade.php
@@ -1,13 +1,36 @@
 @extends('main')
 @section('content')
-    <div class="d-flex flex-wrap inventory">
-        @foreach ($data['rgDescriptions'] as $item)
-
-        <div class="item" style="background-image: url(https://steamcommunity-a.akamaihd.net/economy/image/{{ $item['icon_url'] }});">
-            <p>{{ $item['name'] }}</p>
+    @if ($inventory['success']==true)
+    <div class="d-flex flex-column h-100">
+        <div class="inventory-wrapper">
+            <div class="d-flex flex-wrap inventory">
+                @foreach ($inventory['inventory'] as $item)
+                    <div class="item" >
+                        <div class="itemImage" style="background-image: url({{ $item['icon_url'] }});">
+                            <p class="d-none id">{{ $item['id'] }}</p>
+                            <p>{{ $item['name'] }}</p>
+                            <p class="amount">{{ $item['amount'] }}</p>
+                            <p>{{ $item['price'] }}$</p>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
+        <div>
+            <form method="POST" action="{{ URL::route('depositItems') }}">
+                @csrf
+                <input id="items" class="d-none" type="text">
+                <button>Sell</button>
+            </form>
 
-        @endforeach
+        </div>
     </div>
+    @else
+    <p>You do not have the game or your inventory is private</p>
+    @endif
+
 @endsection
 @section('title',"Inventory")
+
+
+
