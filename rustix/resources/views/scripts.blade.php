d75f64281b8fd9e8bb95cce69d6c9bb16c38b311
TheStrangeProgrammer <48047563+TheStrangeProgrammer@users.noreply.github.com>
2022-01-14 19:36:38 +0200

    Update scripts.blade.php

--- rustix/resources/views/scripts.blade.php
+++ rustix/resources/views/scripts.blade.php
@@ -3,18 +3,30 @@
 <script>
     var items = [];
     $(document).ready(function() {
-        $('.item').click(function() {
-            if($(this).hasClass("itemActive")){
+        $('.item').click(function(event) {
+            if($(event.target).hasClass("item")){
+                if($(this).hasClass("itemActive")){
                 $(this).removeClass("itemActive");
+                $(this).find(".amountInput").addClass("d-none");
                 items = items.filter(it => it.id!=$(this).find(".id").text());
             }else{
                 $(this).addClass("itemActive");
+                $(this).find(".amountInput").removeClass("d-none");
                 var item =new Object();
                 item.id= $(this).find(".id").text();
                 item.amount=$(this).find(".amount").text();
                 items.push(item);
             }
+            }
 
+        })
+        $('.amountInput').change(function() {
+            if($(this).hasClass("itemActive")){
+                items.find(it => it.id);
+                item.amount=$(this).text();
+            }else{
+
+            }
         })
     });
     $(document).ready(function() {
