4031c248b0352cf455fcaf943c183507f5463b67
TheStrangeProgrammer <48047563+TheStrangeProgrammer@users.noreply.github.com>
2022-02-01 02:05:09 +0200

    rebuild

---
+++ resources/js/components/Chat.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="d-flex flex-column">
+        <div class="d-flex flex-fill chat-wrapper">
+            <div v-for="message in messages">
+
+                <message
+                    :message="message.text"
+                    :user="message.user"
+                ></message>
+            </div>
+        </div>
+
+        <form @submit.prevent="submit" class="chat-form">
+            <div class="field has-addons has-addons-fullwidth d-flex p-2">
+                <div class="d-flex flex-fill control is-expanded p-2">
+                    <input class="input chat-input flex-fill" type="text" placeholder="Type a message" v-model="newMessage">
+                </div>
+                <div class="control p-2">
+                    <button type="submit" class="button is-danger" :disabled="!newMessage">
+                        Send
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            user:String
+            },
+        data () {
+            return {
+                messages: [],
+                newMessage: ''
+            }
+        },
+        mounted () {
+            this.fetchMessages();
+            Echo.channel('chat')
+                .listen('NewChatMessage', (e) => {
+                    if(e.user != this.user) {
+                        this.messages.push({
+                            text: e.message,
+                            user: e.user
+                        });
+                    }
+                });
+        },
+        methods: {
+            fetchMessages() {
+                axios.get(`${process.env.MIX_WEBSOCKET_SERVER_BASE_URL}/api/messages`).then(response => {
+                    this.messages = response.data;
+                })
+            },
+
+            submit() {
+                axios.post(`${process.env.MIX_WEBSOCKET_SERVER_BASE_URL}/message`, {
+                    user: this.user,
+                    message: this.newMessage
+                }).then((response) => {
+                    this.messages.push({
+                        text: this.newMessage,
+                        user: this.user
+                    });
+
+                    this.newMessage = '';
+                }, (error) => {
+                    console.log(error);
+                });
+
+            }
+        }
+    }
+</script>
---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,18 @@
+@extends('main')
+@section('content')
+
+    <div class="m-4">
+        <div class="card-game">
+          <div class=" card-img-top-wrapper">
+            <div class="d-flex card-overlay">
+                <a href="{{ URL::route('roulette') }}" class="play-button m-auto py-3" style="color: white;">PLAY</a>
+            </div>
+          </div>
+          <div class="card-body">
+            <h5 class="game-text">ROULETTE</h5>
+          </div>
+        </div>
+    </div>
+
+@endsection
+@section('title', 'Home')
