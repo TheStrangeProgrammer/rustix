bedf876fab19ec45ba8549009f96459539de1ec2
ioneltomi <ioneltomi80@gmail.com>
2022-02-07 13:15:46 +0200

    modif tomi

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,9 +1,9 @@
 @extends('main')
 @section('content')
-
+  <div class="d-flex">
     <div class="m-4">
         <div class="card-game">
-          <div class=" card-img-top-wrapper">
+          <div class=" card-img-top-wrapper" style="background-image: url('../assets/roulette-game.svg'">
             <div class="d-flex card-overlay">
                 <a href="{{ URL::route('roulette') }}" class="play-button m-auto py-3" style="color: white;">PLAY</a>
             </div>
@@ -13,6 +13,18 @@
           </div>
         </div>
     </div>
-
+    <div class="m-4">
+      <div class="card-game">
+        <div class=" card-img-top-wrapper" style="background-image: url('../assets/x-roulette2.svg'">
+          <div class="d-flex card-overlay">
+              <a href="{{ URL::route('x-roulette') }}" class="play-button m-auto py-3" style="color: white;">PLAY</a>
+          </div>
+        </div>
+        <div class="card-body">
+          <h5 class="game-text">X-ROULETTE</h5>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
 @section('title', 'Home')
