a276b8b66119bb5212f01745f2077dbfa8155d16
ioneltomi <ioneltomi80@gmail.com>
2022-02-16 16:24:45 +0200

    modif tomi

--- public/js/layouts/profile.js
+++ public/js/layouts/profile.js
@@ -1,29 +1,10 @@
-$("butn").click(function(){ // when any button is clicked
-    $("butn").removeClass("active"); //delete all "active" classes from all buttons
-    $(this).addClass("active"); //assign the "active" class only to the one that was clicked
-    return false;
+$(document).ready(function() {
+    var pt = $('.p0 span:last-child');
+    var text = parseFloat($(pt).text().replace(',', '.'));    
+
+    if (text < 0) {
+        $(".p0 span:last-child").css({
+            'color': '#AF2901',
+        })
+    }
 });
-
-function showDiv() { 
-    document.getElementById('profile').style.display = "flex";
-    if(showDiv){
-        document.getElementById('referrals').style.display = "none";
-        document.getElementById('history').style.display = "none";
-    }
- }
- function showDiv2() {
-    document.getElementById('referrals').style.display = "flex";
-    if(showDiv2){
-        document.getElementById('profile').style.display = "none";
-        document.getElementById('history').style.display = "none";
-    }
- }
- function showDiv3() {
-    document.getElementById('history').style.display = "flex";
-    if(showDiv3){
-        document.getElementById('profile').style.display = "none";
-        document.getElementById('referrals').style.display = "none";
-
-    }
- }
- 
--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -3,83 +3,85 @@
 
 
 
-    <div class="flex-column flex-container">
+    <div class="box-div">
+        <span class="head-profile text-edit">Your Profile.</span>
+        <div class="box-columns">
+            <div class="text-edit flex-fill col-div">
+                <span class="first-span-edit">Stats</span>
+                <div class="total-deposited text-edit" style="background-color: rgba(53,59,84,1)">
+                    <span class="p-2">Total Deposited:</span>
+                    <span class="ms-auto p-2">$653.44</span>
+                </div>
+                <div class="total-deposited text-edit" >
+                    <span class="p-2">Total Grambled:</span>
+                    <span class="ms-auto p-2">$953.66</span>
+                </div>
+                <div class="total-deposited text-edit p0" style="background-color: rgba(53,59,84,1)">
+                    <span class="p-2 ">Profit:</span>
+                    <span class="ms-auto p-2">
+                        -321,88
+                    </span>
+                </div>
+                <span class="first-span-edit" style="padding: 2.625rem 0">Setting</span>
+                <div class="total-deposited text-edit" style="background-color: rgba(53,59,84,1)">
+                    <span class="p-2">Trade URL:</span>
+                </div>
+                <div class="total-deposited">
+                    <a class="p-2 text-edit" style="opacity: 0.24" href="https://steamcommunity.com/profiles/76561198141872103/tradeoffers">https://steamcommunity.com/profiles/76561198141872103/tradeoffers</a>
+                </div>
+                <div class="total-deposited text-edit" style="background-color: rgba(53,59,84,1)">
+                    <span class="p-2">Help me find my steam trade link</span>
+                    <a class="btn-bet ms-auto text-edit" href="https://steamcommunity.com/profiles/76561198141872103/tradeoffers">Update</a>
+                </div>
+                    <div class="flex-fill avatar-edit"> 
+                        <img src="{{ Auth::user()->avatar }}">
+                        <span class="text-edit">MOD</span>
+                        <span>{{ Auth::user()->name }}</span>
+                    </div>
+            </div>
+            <div class="text-edit flex-fill col-div" style="background-color: #0d0e14">
+                <span class="first-span-edit">Betting History</span>
+            <div class="scroll">
+                <div class="total-deposited text-edit" style="background-color: #00C74D">
+                    <span class="p-2">Won 1337 coins:</span>
+                    <span class="ms-auto p-2">x-roulette</span>
+                    <span class="ms-auto p-2">02:44am</span>
+                </div>
+                <div class="ScrollStyle">
+                  
+                <div class="total-deposited  text-edit" style="background-color: #00C74D">
+                    <span class="p-2">Won 1337 coins:</span>
+                    <span class="ms-auto p-2">roulette</span>
+                    <span class="ms-auto p-2">03:14pm</span>
+                </div>
+                <div class="total-deposited text-edit" style="background-color: rgba(175,41,41,1)">
+                    <span class="p-2">Lost 1337 coins:</span>
+                    <span class="ms-auto p-2">crazytime</span>
+                    <span class="ms-auto p-2">10-03-22</span>
+                </div>
+                <div class="total-deposited text-edit" style="background-color: #00C74D">
+                    <span class="p-2">Won 1337 coins:</span>
+                    <span class="ms-auto p-2">x-roulette</span>
+                    <span class="ms-auto p-2">02:44am</span>
+                </div>
+                <div class="total-deposited text-edit" style="background-color: rgba(175,41,41,1)">
+                    <span class="p-2">Lost 1337 coins:</span>
+                    <span class="ms-auto p-2">x-roulette</span>
+                    <span class="ms-auto p-2">02:44am</span>
+                </div>
+            </div>
+        </div>
+
+            
+    </div>
+    </div>
+
+    <!--
             <div class="p-3"> 
                 <img src="{{ Auth::user()->avatar }}">
                 <span>{{ Auth::user()->name }}</span>
             </div>
-            <div class="head-buttons">
-                <input id="butn" type="button" name="answer" value="Overview" onclick="showDiv()" />
-                <input id="butn" type="button" name="answer" value="Referrals" onclick="showDiv2()" />
-                <input id="butn" type="button" name="answer" value="History" onclick="showDiv3()" />
-            </div>
-            <div class=" profile-edit">        
-            <div id="profile" style="display: none" class="answer_list">               
-                                
-                    <div class="head-information me-2">
-                        <p>Total Deposited:</p>
-                        <p>Total Spent:</p>
-                        <p>Total Withdrawed:</p>
-                        <input name="tradeToken" placeholder="Token here.." type="text" value="{{ $tradeToken }}">
-                    </div>
-                    
-                    <div class="head-information ms-2 ">
-                        <div class="d-flex d-inline">
-                            <img src="assets/dollar_coin.svg">
-                            <p >{{ $totalDeposit }}</p>
-                        </div>
-                        <div class="d-flex d-inline">
-                            <img src="assets/dollar_coin.svg">
-                            <p class="ms-3 py-2 ">{{ $totalSpent }}</p>
-                        </div>
-                        <div class="d-flex d-inline">
-                            <img src="assets/dollar_coin.svg">
-                            <p class="ms-3 py-2 ">{{ $totalWithdraw }}</p>
-                        </div>
-                        <form method="POST" action="{{ URL::route('setTradeToken') }}">
-                            @csrf 
-                            <input class="submit-btn" type="submit" value="Set Token">
-                        </form>
-                    </div>
-            </div>
-            
-        
-        <div class="d-flex"> 
-            
-            <div id="referrals" style="display:none" class="answer_list profile-borders">
-                <div class="head-information me-2">
-                    <p>Your Code: </p>
-                    @if (Auth::user()->referredBy == null)
-                        <form method="POST"
-                            action="{{ URL::route('setReferral') }}">
-                            @csrf
-                            <input name="referrerCode" placeholder="Code here.." type="text">
-                            
-                        </form>
-                    @else
-                        <p>You are already referred to: {{ $referrerName }}</p>
-                    @endif
-                    <p >Users Referred By You:</p>
-                </div>
-                <div class="head-information ms-2">
-                    <p>{{ $referralCode }}</p>
-                        @foreach ($referrals as $referal)
-                            <p class="ms-2 me-2">{{ $referal['name'] }}</p>
-                        @endforeach
-                        <input class="submit-btn" type="submit" value="Set Code">
-                </div>
-            </div>
-            <div id="history" style="display: none" class="answer_list flex-child ">
-
-                <div>
-
-                </div>
-                
-            </div>
-        </div>  
-        </div>
-    </div>
-    
+           -->
     @endsection
     @section('title', 'Profile')
     @section('css')
