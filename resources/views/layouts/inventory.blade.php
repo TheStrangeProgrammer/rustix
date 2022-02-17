856fcbf96fcc350914a32d2a9cb330bf6e4ab648
ioneltomi <ioneltomi80@gmail.com>
2022-02-17 03:46:19 +0200

    asd

--- resources/views/layouts/inventory.blade.php
+++ resources/views/layouts/inventory.blade.php
@@ -1,15 +1,15 @@
 @extends('main')
 @section('content')
     @if ($inventory['success'] == true)
-        <div class="d-flex flex-column flex-fill h-100">
+        <div class="flex-fill principal-div">
             <div class="head-inventory">
                 <span>Your inventory.</span>
             </div>
             <div class="flex-fill inventory-wrapper">
-                <div class="d-flex flex-wrap inventory">
+                <div class="d-flex flex-wrap">
 
                     @foreach ($inventory['inventory'] as $item)
-                        <div class="d-flex flex-column align-items-center m-2 item ">
+                        <div class="item ">
                             <p class="d-none item-id">{{ $item['id'] }}</p>
                             <p class="d-none item-quantity">{{ $item['amount'] }}</p>
 
@@ -18,9 +18,9 @@
 
 
 
-                            <div class="d-flex flex-row w-100 item-info">
-                                <p class="me-auto px-3">Q: {{ $item['amount'] }}</p>
-                                <p class="px-3">$: <span class="item-price">{{ $item['price'] }}</span></p>
+                            <div class="item-info">
+                                <p class="me-auto px-2">Q: {{ $item['amount'] }}</p>
+                                <p class="px-2">$: <span class="item-price">{{ $item['price'] }}</span></p>
                             </div>
 
                             <div class="d-flex">
@@ -35,13 +35,17 @@
                     @endforeach
                 </div>
             </div>
-            <div class="d-flex container-fluid  fw-bold inventory-sell p-2" style="background-color: #141620">
-                <form class="d-inline mx-auto" method="POST" action="{{ URL::route('depositItems') }}">
+            <div class="container-fluid inventory-sell">
+                <form method="POST" action="{{ URL::route('depositItems') }}">
                     @csrf
-                    <p class="text-white d-inline me-5">Total: <span id="total">0</span></p>
+                    <div class="my-auto me-auto ms-5 right-span">
+                        <span class="my-auto">You have selected<span class="span-13" >&nbsp;13</span></span>
+                        <span class="align-items-center my-auto">&nbsp;items worth<span id="total">&nbsp;0</span></span>
+                    </div>
+                   
                     <input id="item-list" class="d-none" type="text" name="itemList">
-                    <input class="text-white px-4 py-2" style="background-color:#14DB1A " id="submit-item-list"
-                        type="submit" value="SELL">
+                    <input class="py-auto me-5 right-span" id="submit-item-list"
+                        type="submit" value="trade for $143.66 coins">
                 </form>
                 </li>
             </div>
