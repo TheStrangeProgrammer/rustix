780460bb4c20f01d6ed9b44afb8f78a9fa7ca362
TheStrangeProgrammer <48047563+TheStrangeProgrammer@users.noreply.github.com>
2022-02-27 14:15:53 +0200

    push

--- public/js/partials/withdraw.js
+++ public/js/partials/withdraw.js
@@ -12,25 +12,26 @@ $("#withdraw-button").click(function (e) {
             let itemToDisplay=`
             <div class="d-flex flex-column align-items-center m-2 item">
                 <span id="item-id" class="d-none">`+item.id+`</span>
-                <span id="item-quantity" class="d-none">`+item.amount+`</span>
                 <span id="item-price" class="d-none">`+item.price+`</span>
 
                 <p class="text-center fw-bold">`+item.name+`</p>
                 <img src="`+item.icon_url+`" >
 
                 <div class="d-flex flex-row justify-content-evenly w-100 item-info">
-                    <p>Q: `+item.amount+`</p>
                     <p>$: <span class="item-price">`+item.price/100+`</span></p>
                 </div>
-                <div class="d-none item-quantity-overlay">
-                    <div class="input-group input-group-sm item-quantity-input">
-                        <span class="input-group-text">Quantity</span>
-                        <input class="form-control" type="number" min="0" max="0" value="0">
-                    </div>
-                </div>
+
             </div>
             `
-            $(".withdraw").append(itemToDisplay);
+            if(item.amount>1){
+                for(let i=0;i<item.amount;i++){
+                    $(".withdraw").append(itemToDisplay);
+                }
+            }else{
+                $(".withdraw").append(itemToDisplay);
+            }
+
+
         });
 
 
@@ -41,44 +42,46 @@ $("#withdraw-button").click(function (e) {
 $("body").on('click', 'div .withdraw .item',function (e) {
     e.preventDefault();
 
-    var inputDiv = $(this).find(".item-quantity-overlay");
-    var infoDiv = $(this).find(".item-info");
     var itemDiv =  $(this);
+
 
     if(itemDiv.hasClass("item-selected")){
         itemDiv.removeClass("item-selected");
-        inputDiv.addClass("d-none");
-        infoDiv.addClass("flex-row");
-
 
         var id = itemDiv.find("#item-id").text();
-
         var itemIndex = itemsToSell.findIndex(element => element.id == id);
-        itemsToSell.splice(itemIndex,1);
+        if(itemsToSell[itemIndex].amount<=1){
+            itemsToSell.splice(itemIndex,1);
+        }else{
+            itemsToSell[itemIndex].amount-=1;
+        }
+
 
     } else {
         itemDiv.addClass("item-selected");
-        inputDiv.removeClass("d-none");
-        inputDiv.find(".form-control").attr("max",itemDiv.find("#item-quantity").text());
-        inputDiv.find(".form-control").attr("value",1);
-        infoDiv.removeClass("flex-row");
-
         var id = itemDiv.find("#item-id").text();
-        var item = new Object();
-        item.id=id;
-        item.amount=1;
-        item.price=itemDiv.find("#item-price").text();
-        itemsToSell.push(item)
+        var itemIndex = itemsToSell.findIndex(element => element.id == id);
+        if(itemIndex!=-1){
+            itemsToSell[itemIndex].amount+=1;
+        }else{
+            var item = new Object();
+
+            item.id=id;
+            item.amount=1;
+            item.price=itemDiv.find("#item-price").text();
+            itemsToSell.push(item);
+        }
 
     }
-    $("#withdraw-total").html( "$"+Math.round(Object.values(itemsToSell).reduce((p,c)=>p+c.amount*c.price/100,0)*100)/100);
-    $("#withdraw-coins").html( Object.values(itemsToSell).reduce((p,c)=>p+c.amount*c.price,0));
-    $("#withdraw-count").html( Object.values(itemsToSell).reduce((p,c)=>p+c.amount,0));
+
+    console.log(itemsToSell);
+
+    $("#withdraw-total").html("$"+Math.round(Object.values(itemsToSell).reduce((p,c)=>p+c.amount*c.price/100,0)*100)/100);
+    $("#withdraw-coins").html(Object.values(itemsToSell).reduce((p,c)=>p+c.amount*c.price,0));
+    $("#withdraw-count").html(Object.values(itemsToSell).reduce((p,c)=>p+c.amount,0));
 
 });
-$("body").on('click',".withdraw .item .item-quantity-input",function (e) {
-    e.stopPropagation();
-});
+
 $("body").on('change',".withdraw .item-quantity-input .form-control",function (e) {
     e.preventDefault();
 
--- resources/views/partials/header/withdraw.blade.php
+++ resources/views/partials/header/withdraw.blade.php
@@ -13,15 +13,16 @@
                     </div>
                 </div>
                 <div class="withdraw-sell justify-content-between flex-wrap align-items-center" style="background-color: #141620">
-                    <div class="d-flex">
-                        <span class="first-text">You have selected<span id="withdraw-count"> 13 </span></span>
+                    <div class="d-flex ">
+                        <span class="first-text">You have selected<span id="withdraw-count">13</span></span>
                         <span class="first-text">items worth<span id="withdraw-total"> $143.66</span></span>
                     </div>
-                    
                     <div class="submit-btn">
                         <button id="submit-withdraw-item-list" class="text-white" id="submit-item-list"
-                            type="submit">trade for <span id="withdraw-coins"> $143.66</span> coins</button>
+                        type="submit">trade for <span id="withdraw-coins"> $143.66</span> coins</button>
                     </div>
+
+
 
                 </div>
             </div>
