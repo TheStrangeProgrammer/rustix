7aa4952550565472504630651f14a592f59f6101
ioneltomi <ioneltomi80@gmail.com>
2022-02-27 19:24:27 +0200

    modif tomi

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -12,7 +12,7 @@
                 </balance>
             </div>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                <button class="navbar-toggler btn1" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
