682f15b5f8500be94ede8144ab52935fc339c9d0
Zet5798 <98855153+Zet5798@users.noreply.github.com>
2022-03-02 21:06:09 +0100

    referrals 80%
    me voy a cocinar

--- resources/views/partials/header/referrals.blade.php
+++ resources/views/partials/header/referrals.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('css/partials/referrals.css') }}">
 <div id="REFERRALS" class="modal fade" tabindex="-1" aria-labelledby="REFERRALS-title" aria-hidden="true">
-    <div class="container modal-dialog modal-xl theme-bc-2">
+    <div class="big modal-dialog modal-xl theme-bc-2">
         <div class="col-cont modal-content theme-bc-2">
             <div class="title modal-header theme-bc-4">
                 <h5 id="PROFILE-title" class="modal-title">Refferals</h5>
@@ -14,82 +14,94 @@
                     </div>
 
                     <div class="col-set text-edit">
-                        <label class="set-ref" for="fname">Your referral code:</label>
-                        <input class="set-code" type="text" id="fname" name="fname">
-                        <input class="set-codeb" type="submit" value="Update">
+                        <div class="set-ref-box">
+                            <label class="set-ref" for="fname">Your referral code:</label>
+                        </div>
+                        <div class="set-code-box">
+                            <input class="set-code" type="text" id="fname" name="fname">
+                        </div>
+                        <div class="set-codeb-box">
+                            <input class="set-codeb" style="background-color:#00C74D" type="submit" value="Update">
+                        </div>
                     </div>
 
                     <div class="col-ins text-edit">
-                        <label class="ins-ref" for="lname">Claim a referral code:</label>
-                        <input class="ins-code" type="text" id="lname" name="lname">
-                        <input class="ins-codeb" type="submit" value="Claim">
+                        <div class="ins-ref-box">
+                            <label class="ins-ref" for="lname">Claim a referral code:</label>
+                        </div>
+                        <div class="ins-code-box">
+                            <input class="ins-code" type="text" id="lname" name="lname">
+                        </div>
+                        <div class="ins-codeb-box">
+                            <input class="ins-codeb" style="background-color:#00C74D" type="submit" value="Claim">
+                        </div>
                     </div>
                 </div>
                 <div class="col-list-2 text-edit " style="background-color: #1A1D29">
                     <div class="row first-span-edit mx-auto">
-                        <span class="title-3 my-auto">Referrals List</span>
+                        <span class="title-3 my-auto">Your earnings</span>
                     </div>
                     <div class="column-scroll scroll">
-                        <div class="ref-list text-edit  " style="background-color: #353B54;">
+                        <div class="ref-list text-edit  " >
                             <span class="Name">Nume Referral</span>
-                            <span class="Time ">Timp Online</span>
+                            <span class="Coins ">100 Coins</span>
                         </div>
 
-                        <div class="ref-list  text-edit" style="background-color: #141620">
+                        <div class="ref-list  text-edit">
                             <span class="Name">Nume Referral</span>
-                            <span class="Time ">Timp Online</span>
+                            <span class="Coins ">100 Coins</span>
                         </div>
-                        <div class="ref-list text-edit  " style="background-color: #353B54;">
+                        <div class="ref-list text-edit  " >
                             <span class="Name">Nume Referral</span>
-                            <span class="Time ">Timp Online</span>
+                            <span class="Coins ">100 Coins</span>
                         </div>
-                        <div class="ref-list text-edit  " style="background-color: #141620;">
+                        <div class="ref-list text-edit  " >
                             <span class="Name">Nume Referral</span>
-                            <span class="Time ">Timp Online</span>
-                        </div>
-
-                        <div class="ref-list  text-edit" style="background-color: #353B54">
-                            <span class="Name">Nume Referral</span>
-                            <span class="Time ">Timp Online</span>
-                        </div>
-                        <div class="ref-list text-edit  " style="background-color: #141620;">
-                            <span class="Name">Nume Referral</span>
-                            <span class="Time ">Timp Online</span>
-                        </div>
-                        <div class="ref-list text-edit  " style="background-color: #353B54;">
-                            <span class="Name">Nume Referral</span>
-                            <span class="Time ">Timp Online</span>
+                            <span class="Coins ">100 Coins</span>
                         </div>
 
-                        <div class="ref-list  text-edit" style="background-color: #141620">
+                        <div class="ref-list  text-edit" >
                             <span class="Name">Nume Referral</span>
-                            <span class="Time ">Timp Online</span>
+                            <span class="Coins ">100 Coins</span>
                         </div>
-                        <div class="ref-list text-edit  " style="background-color: #353B54;">
+                        <div class="ref-list text-edit  " >
                             <span class="Name">Nume Referral</span>
-                            <span class="Time ">Timp Online</span>
+                            <span class="Coins ">100 Coins</span>
                         </div>
-                        <div class="ref-list text-edit  " style="background-color: #141620;">
+                        <div class="ref-list text-edit  " >
                             <span class="Name">Nume Referral</span>
-                            <span class="Time ">Timp Online</span>
-                        </div>
-
-                        <div class="ref-list  text-edit" style="background-color: #353B54">
-                            <span class="Name">Nume Referral</span>
-                            <span class="Time ">Timp Online</span>
-                        </div>
-                        <div class="ref-list text-edit  " style="background-color: #141620;">
-                            <span class="Name">Nume Referral</span>
-                            <span class="Time ">Timp Online</span>
-                        </div>
-                        <div class="ref-list text-edit  " style="background-color: #353B54;">
-                            <span class="Name">Nume Referral</span>
-                            <span class="Time ">Timp Online</span>
+                            <span class="Coins ">100 Coins</span>
                         </div>
 
-                        <div class="ref-list  text-edit" style="background-color: #141620">
+                        <div class="ref-list  text-edit" >
                             <span class="Name">Nume Referral</span>
-                            <span class="Time ">Timp Online</span>
+                            <span class="Coins ">100 Coins</span>
+                        </div>
+                        <div class="ref-list text-edit  " >
+                            <span class="Name">Nume Referral</span>
+                            <span class="Coins ">100 Coins</span>
+                        </div>
+                        <div class="ref-list text-edit  " >
+                            <span class="Name">Nume Referral</span>
+                            <span class="Coins ">100 Coins</span>
+                        </div>
+
+                        <div class="ref-list  text-edit" >
+                            <span class="Name">Nume Referral</span>
+                            <span class="Coins ">100 Coins</span>
+                        </div>
+                        <div class="ref-list text-edit  " >
+                            <span class="Name">Nume Referral</span>
+                            <span class="Coins ">100 Coins</span>
+                        </div>
+                        <div class="ref-list text-edit  " >
+                            <span class="Name">Nume Referral</span>
+                            <span class="Coins ">100 Coins</span>
+                        </div>
+
+                        <div class="ref-list  text-edit" >
+                            <span class="Name">Nume Referral</span>
+                            <span class="Coins ">100 Coins</span>
                         </div>
                     </div>
 
