8654e610d1e128319f5d5e7c0d54505e7fa25ec5
ioneltomi <ioneltomi80@gmail.com>
2022-03-04 19:33:16 +0200

    Modif tomi

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,18 +1,19 @@
 @extends('main')
 @section('content')
-    <div class="d-flex flex-fill flex-column">
+    <div class="d-flex w-100 flex-column">
         <div class="head-text">
             <span class="welcome-text">Welcome back, <span class="my-auto align-self-center">{{ Auth::user()->name }}</span>
             </span>
             <span class="small-text">have a great day</span>
         </div>
             <span class="gamemodes-text">GAMEMODES:</span>
+    <a class="cola" href="{{ URL::route('roulette') }}">
     <div class="games-div">
         <div class="card-game">
-            <div class="d-flex p-3">
+            <div class="top-card">
                 <h5 class="game-text">ROULETTE</h5>
             </div>
-            <div class="card-img-top-wrapper" style="background-image: url('../assets/roulette-game.svg'">
+            <div class="card-img-top-wrapper" style="background-image: url('../assets/Capture.svg'">
                 <div class="d-flex card-overlay">
                     <a href="{{ URL::route('roulette') }}" class="play-button m-auto" style="color: white;">PLAY</a>
                 </div>
@@ -20,10 +21,10 @@
         </div>
 
         <div class="card-game">
-            <div class="d-flex p-3">
+            <div class="top-card">
                 <h5 class="game-text">X-ROULETTE</h5>
             </div>
-            <div class=" card-img-top-wrapper" style="background-image: url('../assets/x-roulette2.svg'">
+            <div class=" card-img-top-wrapper" style="background-image: url('../assets/Capture2.svg'">
                 <div class="d-flex card-overlay">
                     <a href="{{ URL::route('x-roulette') }}" class="play-button m-auto"
                         style="color: white;">PLAY</a>
@@ -32,6 +33,7 @@
             
         </div>
     </div>
+</a>
     </div>
 @endsection
 @section('title', 'Home')
