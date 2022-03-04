ab706b30f46fbac28318d1fb80a5d71f27d186f9
TheStrangeProgrammer <48047563+TheStrangeProgrammer@users.noreply.github.com>
2022-03-04 19:47:30 +0200

    admin

--- resources/views/partials/header/admin.blade.php
+++ resources/views/partials/header/admin.blade.php
@@ -1,6 +1,18 @@
 
 <link rel="stylesheet" type="text/css" href="{{ asset('css/layouts/admin.css') }}">
-<div class="d-flex m-5 cola">asdf</div>
+<div id="ADMIN" class="modal fade" tabindex="-1" aria-labelledby="ADMIN-title" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 id="ADMIN-title" class="modal-title">Your admin.</h5>
+                <button type="button" class="btn-close color-btn" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body justify-content-center">
+
+            </div>
+        </div>
+    </div>
+</div>
 
 @push('js')
     <script src="{{ asset('js/layouts/admin.js') }}"></script>
